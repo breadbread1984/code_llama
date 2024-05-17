82890353e0eedda20ebc5977a9c1a98b477f6727
breadbread1984 <breadbread1984@163.com>
2024-05-17 15:05:50 +0800

    update

--- main.py
+++ main.py
@@ -15,31 +15,19 @@ def add_options():
 def CodeLlama(locally = False):
   login(token = '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')
   tokenizer = AutoTokenizer.from_pretrained('meta-llama/CodeLlama-7b-Instruct-hf')
-  if locally:
-    llm = HuggingFacePipeline.from_model_id(
-      model_id = 'meta-llama/CodeLlama-7b-Instruct-hf',
-      task = 'text-generation',
-      device = 0,
-      pipeline_kwargs = {
-        "max_length": 16384,
-        "do_sample": False,
-        "temperature": 0.8,
-        "top_p": 0.8,
-        "use_cache": True,
-        "return_full_text": False
-      }
-    )
-  else:
-    environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
-    llm = HuggingFaceEndpoint(
-      endpoint_url = 'meta-llama/CodeLlama-7b-Instruct-hf',
-      task = 'text-generation',
-      max_length = 16384,
-      do_sample = False,
-      temperature = 0.8,
-      top_p = 0.8,
-      use_cache = True
-    )
+  llm = HuggingFacePipeline.from_model_id(
+    model_id = 'meta-llama/CodeLlama-7b-Instruct-hf',
+    task = 'text-generation',
+    device = 0,
+    pipeline_kwargs = {
+      "max_length": 16384,
+      "do_sample": False,
+      "temperature": 0.8,
+      "top_p": 0.8,
+      "use_cache": True,
+      "return_full_text": False
+    }
+  )
   return tokenizer, llm
 
 def main(unused_argv):
